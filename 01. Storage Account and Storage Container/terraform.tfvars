f66e0e16da22ab99e1613ec6a53101ca10f23e5b
il5 <il5kv@outlook.com>
2026-01-20 22:27:46 +0200

    Deploy Storage Account and Storage Container for dev and prod

---
+++ 01. Storage Account and Storage Container/terraform.tfvars
@@ -0,0 +1,2 @@
+application_name = "app"
+primary_region   = "northeurope"
---
+++ 01. Storage Account and Storage Container/versions.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.54.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "abcc12e8-46f6-4fb0-ae1f-86a92dafdaa7"
+}
